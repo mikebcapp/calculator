4610f882ff9a87389be460289d32314c87bc17f5
mikebcapp <mikbcapp@gmail.com>
2015-03-18 23:13:46 +1300

    Refactored calculator assignment

--- calc.rb
+++ calc.rb
@@ -1,35 +1,26 @@
-
-
 puts "Welcome to my command line calculator"
 puts "--------------------------------------"
 begin
-  puts "Enter your first number"
-  num1 = gets.chomp 
-  puts "You entered the following number: #{num1}"
-  
-  puts "Enter a second number"
-  num2 = gets.chomp
-  puts "You entered the following number: #{num2}"
-  
-  puts "Choose 1) to add  2) to subtract 3) to multiply or 4) to divide the two numbers"
-  operator = gets.chomp
-    case 
-      when operator == "1"
-        result = num1.to_i + num2.to_i
-        p "The answer is #{result}"
-      when operator == "2"
-        result = num1.to_i - num2.to_i
-        p "The answer is #{result}"
-      when operator =="3"
-        result = num1.to_i * num2.to_i
-        p "The answer is #{result}"
-      when operator == "4"
-        result = num1.to_f / num2.to_f
-        p "The answer is #{result}"
-
-      else puts "Please enter 1,2,3,or 4"
-   
+  def user_input(num_order)
+    puts "Enter your #{num_order} number into the calculator"
+    input = gets.chomp
+    puts "You entered the following number:  #{input}"
+    input  
+  end
+  num1 = user_input("first")
+  num2 = user_input("second")
+  begin
+    puts "Choose 1) to add  2) to subtract 3) to multiply or 4) to divide the two numbers"
+    operator = gets.chomp
+  end until %w(1 2 3 4).include?(operator)
+  result = case operator
+    when "1" then num1.to_i + num2.to_i
+    when "2" then num1.to_i - num2.to_i
+    when "3" then num1.to_i * num2.to_i
+    when "4" then num1.to_f / num2.to_f
+    else puts "Please enter 1,2,3,or 4"
     end
+  puts "The answer is #{result}"
   puts 'Do you want to continue? Enter Yes to continue and carry on calculating'
-  answer = gets.chomp
-end while answer == 'Yes'
+  answer = gets.chomp.downcase
+end while answer == 'yes'
